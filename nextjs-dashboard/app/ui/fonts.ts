6e4cdc3a7391c11c7605b6009ba01bab9b138c55
Darique <dwarrennyc@protonmail.com>
2025-02-10 00:20:52 -0500

    finished chapter 3

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({subsets: ['latin'], weight:'700',  } )
+export const lusitana = Lusitana({
+    weight: ['400','700'], 
+    subsets:['latin'],})
