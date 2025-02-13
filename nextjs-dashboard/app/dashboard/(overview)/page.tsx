01b33e0e7bd73e031ba44318228a3f340b99c9e2
Darique <dwarrennyc@protonmail.com>
2025-02-13 18:04:13 -0500

    installed nextjs@canary

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,7 +2,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import {  fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import CardWrapper from '@/app/ui/dashboard/cards'
 import { 
@@ -11,7 +11,6 @@ import {
   CardSkeleton } from '@/app/ui/skeletons';
 
  export default async function Page() {
-    const latestInvoices = await fetchLatestInvoices()
     const {
       totalPaidInvoices, 
       totalPendingInvoices, 
