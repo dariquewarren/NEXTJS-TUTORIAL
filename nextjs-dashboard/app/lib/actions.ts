f88f81b62f5d4de9f8e3f170ea30f6ff7d39b0fc
Darique <dwarrennyc@protonmail.com>
2025-02-16 22:46:21 -0500

    added try catch blocks to action functions

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -25,13 +25,17 @@ export async function createInvoice(formData: FormData){
 
    const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0];
-
-   await sql `
-   INSERT INTO invoices (customer_id, amount,status, date)
-   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
-   ;
-   revalidatePath('/dashboard/invoices');
-   redirect('/dashboard/invoices');
+try {
+    await sql `
+    INSERT INTO invoices (customer_id, amount,status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
+    ;
+    
+} catch (error) {
+    console.log(`error found: ${error}`)
+}
+  revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 
    
 }
@@ -45,24 +49,35 @@ export async function updateInvoice (id: string, formData: FormData){
         status: formData.get('status')
     })
     const amountInCents = amount *100
-
+try {
     await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents} status= ${status}
     WHERE id = ${id}
-    `
-    ;
-
-    revalidatePath('/dashboard/invoices');
-   redirect('/dashboard/invoices');
+    `;
+    
+} catch (error) {
+    console.log(`error found: ${error}`)
 
 }
-const DeleteInvoice = FormSchema.omit({id:true,date:true})
-export async function deleteInvoice (id:string, formData: FormData){
+   revalidatePath('/dashboard/invoices');
+   redirect('/dashboard/invoices');
+    
 
-await sql `
-DELETE FROM invoices
-WHERE id = ${id}
-`
-revalidatePath('/dashboard/invoices');
+    
+
+}
+export async function deleteInvoice (id:string){
+    throw new Error('Failed to delete invoice')
+try {
+    await sql `
+    DELETE FROM invoices
+    WHERE id = ${id}
+    `
+} catch (error) {
+    console.log(`error found: ${error}`)
+
+}
+    revalidatePath('/dashboard/invoices'); 
+
 }
