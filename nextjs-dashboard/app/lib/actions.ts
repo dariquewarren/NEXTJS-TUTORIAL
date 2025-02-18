eb9586289ce1c0dc162ffe4c299693723c5c7883
Darique <dwarrennyc@protonmail.com>
2025-02-17 20:14:03 -0500

    started adding auth the nextjs way

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -4,6 +4,8 @@ import {z} from 'zod'
 import postgres from 'postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from 'auth'
+import { AuthError } from 'next-auth'
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'})
 
@@ -103,5 +105,26 @@ try {
 
 }
     revalidatePath('/dashboard/invoices'); 
+
+}
+
+export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData
+){
+    try {
+        await signIn('credentials', formData)
+    } catch (error) {
+        if (error instanceof AuthError){
+            switch (error.type){
+                case 'CredentialsSignin':
+                    return 'Invalid Credentials';
+                    default:
+                        return 'Something went HORRIBLY WrOnG'
+
+            }
+        }
+        throw error
+    }
 
 }
